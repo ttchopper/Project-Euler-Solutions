080e44112c5f21c97bf7f9f1ce4f8cc478df0560
andrew <twinklechopper@gmail.com>
2016-02-11 22:31:48 +0200

    added function solution

--- largest_prime_factor.js
+++ largest_prime_factor.js
@@ -8,3 +8,18 @@ while (smallestPrime != num) {
          smallestPrime++;
     }
 }
+
+
+//Function
+function findLargestPrime(number) {
+   var smallestPrime = 2;
+   while (smallestPrime != number) {
+       if (number % smallestPrime === 0) {
+   	       number = number / smallestPrime;
+ 	     }
+       else {
+           smallestPrime++;
+       }
+   }
+   return number;
+}
